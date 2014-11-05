98f2bd3a1707136fe5bd32babe6bbc9dc6e680c2
Jeff Donahue <jeff.donahue@gmail.com>
2014-11-05 13:12:53 -0800

    AccuracyLayer: add 'denominator' param

--- include/caffe/loss_layers.hpp
+++ include/caffe/loss_layers.hpp
@@ -92,6 +92,8 @@ class AccuracyLayer : public Layer<Dtype> {
   int ignore_label_;
   /// Keeps counts of the number of samples per class.
   Blob<Dtype> nums_buffer_;
+
+  Dtype denominator_;
 };
 
 /**
